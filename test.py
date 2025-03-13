f1aff9f07697c0b0216230bb69cbe94230a4cc27
Srimadhav Seebu Kumar <srimadhavseebukumar@gmail.com>
2025-03-13 17:11:23 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,12 @@
 from ultralytics import YOLO
 
-# Load the trained YOLO classification model
-model = YOLO("best.pt")  # Update the path if needed
+model = YOLO("best.pt")  
 
-# Test on a single image
-image_path = "images (50).jpg"  # Replace with your test image path
+image_path = "images (50).jpg"  
 results = model(image_path)
 
-# Print results
 print(results)
 
-# Get predicted class
 predicted_class = results[0].probs.top1
-class_names = model.names  # Get class names from the model
+class_names = model.names 
 print(f"Predicted class: {class_names[predicted_class]}")
